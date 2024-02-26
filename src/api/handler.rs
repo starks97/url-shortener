a621519172a555db1387c92beb92892b6ba16b91
Jefferson <ifrit68@hotmail.com>
2024-02-26 18:35:56 -0500

    feature[new CORS config]

--- src/api/handler.rs
+++ src/api/handler.rs
@@ -1,4 +1,5 @@
-use actix_web::web;
+use actix_cors::Cors;
+use actix_web::{http::header, web};
 
 use super::auth::{
     create_user, get_me_handler, get_users, logout_handler, refresh_access_token_handler,
@@ -9,6 +10,20 @@ use super::url::{
 };
 
 pub fn config_handler(config: &mut web::ServiceConfig) {
+    let cors = Cors::default()
+        .allowed_origin("http://127.0.0.1:4323")
+        .allowed_methods(vec!["GET", "POST", "PUT", "PATCH"]) // Set allowed HTTP methods
+        .allowed_headers(vec![
+            header::CONTENT_TYPE,
+            header::ACCEPT,
+            header::HeaderName::from_static("sentry-trace"),
+            header::HeaderName::from_static("baggage"),
+        ]) // Set allowed headers
+        .allowed_header(header::CONTENT_TYPE) // Set allowed headers
+        .expose_headers(&[header::CONTENT_DISPOSITION])
+        .supports_credentials()
+        .max_age(3600);
+
     let scope = web::scope("/api")
         .service(get_users)
         .service(create_user)
@@ -20,7 +35,8 @@ pub fn config_handler(config: &mut web::ServiceConfig) {
         .service(update_url)
         .service(delete_url)
         .service(get_url_by_id)
-        .service(redirect_to_original_url);
+        .service(redirect_to_original_url)
+        .wrap(cors);
 
     config.service(scope);
 }
